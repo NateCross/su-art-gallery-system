1e93058e23621da453cf64d173b5e91430b8f135
Nathan Angelo B. Cruz <jonathan.a.b.cross@gmail.com>
2022-12-02 21:58:26 +0800

    Changed primary color to SOUL red

--- tailwind.config.js
+++ tailwind.config.js
@@ -26,21 +26,33 @@ module.exports = {
     require('tailwindcss-textshadow'),
   ],
 
-  // daisyui: {
-  //   themes: [
-  //     {
-  //       soap: {
-  //         "primary": "#CC0000",
-  //         "secondary": "#F000B8",
-  //         "accent": "#37CDBE",
-  //         "neutral": "#3D4451",
-  //         "base-100": "#FFFFFF",
-  //         "info": "#3ABFF8",
-  //         "success": "#36D399",
-  //         "warning": "#FBBD23",
-  //         "error": "#F87272",
-  //       },
-  //     },
-  //   ],
-  // },
+  daisyui: {
+    themes: [
+      {
+        light: {
+          ...require('daisyui/src/colors/themes')['[data-theme=light]'],
+          primary: '#cc0000',
+        },
+
+      },
+      {
+        dark: {
+          ...require('daisyui/src/colors/themes')['[data-theme=dark]'],
+          primary: '#cc0000',
+        }
+
+        // soap: {
+        //   "primary": "#CC0000",
+        //   "secondary": "#F000B8",
+        //   "accent": "#37CDBE",
+        //   "neutral": "#3D4451",
+        //   "base-100": "#FFFFFF",
+        //   "info": "#3ABFF8",
+        //   "success": "#36D399",
+        //   "warning": "#FBBD23",
+        //   "error": "#F87272",
+        // },
+      },
+    ],
+  },
 };
